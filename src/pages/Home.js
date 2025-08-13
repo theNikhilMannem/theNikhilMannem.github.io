e565b11c808520a044d8dfa4dc236566304b6e8a
Nikhil K Mannem <thenikhilmannem@gmail.com>
2025-08-13 11:21:00 +0530

    feat: another new github-pages site

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Nikhil Mannem's GitHub Pages Site 🚀</h1>
+      <p>This is a sample React project optimized for GitHub Pages.</p>
+    </div>
+  );
+}
